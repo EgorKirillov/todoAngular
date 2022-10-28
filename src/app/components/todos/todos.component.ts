a3e11ab5dd69f02cdefd1f86ca18d451af8a4c4e
EgorKirillov <80296253508@mail.ru>
2022-10-28 19:59:06 +0300

    add: catch http errors

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { TodoList, TodosService } from '../../services/todos.service'
+import { HttpErrorResponse } from '@angular/common/http'
 
 @Component({
   selector: 'tdl-todos',
@@ -10,27 +11,43 @@ export class TodosComponent implements OnInit {
   constructor(private todosService: TodosService) {}
 
   todos: TodoList[] = []
+  error = ''
 
   createTodosHandler() {
     console.log('create TODO with title')
     const title = `NG${Math.floor(Math.random() * 100)}`
-    this.todosService.createTodos(title).subscribe(res => {
-      this.todos.push(res.data.item)
+    this.todosService.createTodos(title).subscribe({
+      next: res => {
+        this.todos.push(res.data.item)
+      },
+      error: (err: HttpErrorResponse) => {
+        this.error = err.message
+      },
     })
   }
 
   deleteTodosHandler() {
     console.log('delete TODO by ID')
-    const deletedID = '881aa5f5-97eb-4632-9c76-c761a33c519c'
-    this.todosService.deleteTodos(deletedID).subscribe(() => {
-      this.todos = this.todos.filter(td => td.id !== deletedID)
+    const deletedID = '104dfe81-e33a-4701-8736-a41ef2920cbb'
+    this.todosService.deleteTodos(deletedID).subscribe({
+      next: () => {
+        this.todos = this.todos.filter(td => td.id !== deletedID)
+      },
+      error: (err: HttpErrorResponse) => {
+        this.error = err.message
+      },
     })
   }
 
   ngOnInit(): void {
     console.log('start todos')
-    this.todosService.getTodos().subscribe(res => {
-      this.todos = res
+    this.todosService.getTodos().subscribe({
+      next: res => {
+        this.todos = res
+      },
+      error: (err: HttpErrorResponse) => {
+        this.error = err.message
+      },
     })
   }
 }
