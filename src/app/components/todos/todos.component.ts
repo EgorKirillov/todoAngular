402e04b93d0fda39d666984661c2252ac8ef23ee
EgorKirillov <80296253508@mail.ru>
2022-10-28 14:40:36 +0300

    feature: create and  delete todos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,6 +8,18 @@ interface TodoList {
   title: string
 }
 
+interface FieldError {
+  field: string
+  error: string
+}
+
+interface TodoResponce<T = {}> {
+  resultCode: number
+  messages: Array<string>
+  fieldsErrors?: Array<FieldError>
+  data: T
+}
+
 @Component({
   selector: 'tdl-todos',
   templateUrl: './todos.component.html',
@@ -18,21 +30,49 @@ export class TodosComponent implements OnInit {
 
   todos: TodoList[] = []
 
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'API-KEY': '4f6e1a9b-a442-4dac-9b05-ef297eadf4f8',
+    },
+  }
+
+  httpBaseUrl = 'https://social-network.samuraijs.com/api/1.1'
+
+  createTodosHandler() {
+    console.log('create TODO')
+    this.createTodos()
+  }
+
+  deleteTodosHandler() {
+    console.log('delete TODO')
+    this.deleteTodos()
+  }
+
   ngOnInit(): void {
     console.log('start')
     this.getTodos()
   }
 
   getTodos() {
+    this.http.get<TodoList[]>(`${this.httpBaseUrl}/todo-lists`, this.httpOptions).subscribe(res => {
+      this.todos = res
+    })
+  }
+
+  createTodos() {
+    const title = `NG${Math.floor(Math.random() * 100)}`
     this.http
-      .get<TodoList[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-        withCredentials: true,
-        headers: {
-          'API-KEY': '4f6e1a9b-a442-4dac-9b05-ef297eadf4f8',
-        },
-      })
+      .post<TodoResponce<{ item: TodoList }>>(`${this.httpBaseUrl}/todo-lists`, { title }, this.httpOptions)
       .subscribe(res => {
-        this.todos = res
+        this.todos.push(res.data.item)
       })
+  }
+
+  deleteTodos() {
+    const deletedID = '3727bd66-174a-4b76-bd0d-c57d066cf400'
+    this.http.delete<TodoResponce>(`${this.httpBaseUrl}/todo-lists/${deletedID}`, this.httpOptions).subscribe(res => {
+      this.todos = this.todos.filter(td => td.id !== deletedID)
+    })
   }
 }
