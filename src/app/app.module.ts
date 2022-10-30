7daea9ad21b6875fa2d82158bda61ff336188a2f
EgorKirillov <80296253508@mail.ru>
2022-10-30 22:05:40 +0300

    add: login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TodosComponent } from './components/todos/todos.component'
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, TodosComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+
+@Component({
+  selector: 'tdl-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  loginForm = new FormGroup({
+    // email: new FormControl('init email'),
+    // password: new FormControl(''),
+  })
+
+  onSubmit() {
+    // console.warn(this.loginForm.value)
+  }
+
+  ngOnInit(): void {
+    console.log('login')
+  }
+}
