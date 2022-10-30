42e8c01686f07d5ca9deb37468a44351cf03c8ab
EgorKirillov <80296253508@mail.ru>
2022-10-30 19:31:08 +0300

    refactor: errors and getTodos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { TodoList, TodosService } from '../../services/todos.service'
-import { HttpErrorResponse } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'tdl-todos',
@@ -10,44 +10,28 @@ import { HttpErrorResponse } from '@angular/common/http'
 export class TodosComponent implements OnInit {
   constructor(private todosService: TodosService) {}
 
-  todos: TodoList[] = []
+  todos$!: Observable<TodoList[]>
   error = ''
 
   createTodosHandler() {
     console.log('create TODO with title')
     const title = `NG${Math.floor(Math.random() * 100)}`
-    this.todosService.createTodos(title).subscribe({
-      next: res => {
-        this.todos.push(res.data.item)
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.message
-      },
-    })
+    this.todosService.createTodos(title)
   }
 
   deleteTodosHandler() {
     console.log('delete TODO by ID')
-    const deletedID = '104dfe81-e33a-4701-8736-a41ef2920cbb'
-    this.todosService.deleteTodos(deletedID).subscribe({
-      next: () => {
-        this.todos = this.todos.filter(td => td.id !== deletedID)
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.message
-      },
-    })
+    const deletedID = 'ecc31dd7-cd52-4c34-bdb2-f6b0034353b5'
+    this.todosService.deleteTodos(deletedID)
+  }
+
+  getTodos() {
+    this.todosService.getTodos()
   }
 
   ngOnInit(): void {
     console.log('start todos')
-    this.todosService.getTodos().subscribe({
-      next: res => {
-        this.todos = res
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.message
-      },
-    })
+    this.todos$ = this.todosService.todos$
+    this.getTodos()
   }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { BehaviorSubject, catchError, EMPTY, map } from 'rxjs'
 import { environment } from '../../environments/environment'
 
 export interface TodoList {
@@ -28,6 +28,8 @@ interface TodoResponce<T = {}> {
 export class TodosService {
   constructor(private http: HttpClient) {}
 
+  todos$: BehaviorSubject<TodoList[]> = new BehaviorSubject<TodoList[]>([])
+
   httpOptions = {
     withCredentials: true,
     headers: {
@@ -35,19 +37,47 @@ export class TodosService {
     },
   }
 
-  getTodos(): Observable<TodoList[]> {
-    return this.http.get<TodoList[]>(`${environment.baseURL}/todo-lists`, this.httpOptions)
+  private static errorHandler(err: HttpErrorResponse) {
+    console.log(err.message)
+    return EMPTY
+  }
+
+  getTodos() {
+    this.http
+      .get<TodoList[]>(`${environment.baseURL}/todo-lists`, this.httpOptions)
+      .pipe(catchError(TodosService.errorHandler.bind(this)))
+      .subscribe(todos => {
+        this.todos$.next(todos)
+      })
   }
 
   createTodos(title: string) {
-    return this.http.post<TodoResponce<{ item: TodoList }>>(
-      `${environment.baseURL}/todo-lists`,
-      { title },
-      this.httpOptions
-    )
+    this.http
+      .post<TodoResponce<{ item: TodoList }>>(`${environment.baseURL}/todo-lists`, { title }, this.httpOptions)
+      .pipe(
+        catchError(TodosService.errorHandler.bind(this)),
+        map(res => {
+          const newTodo = res.data.item
+          const stateTodos = this.todos$.getValue()
+          return [...stateTodos, newTodo]
+        })
+      )
+      .subscribe(todos => {
+        this.todos$.next(todos)
+      })
   }
 
   deleteTodos(deletedID: string) {
-    return this.http.delete<TodoResponce>(`${environment.baseURL}/todo-lists/${deletedID}`, this.httpOptions)
+    return this.http
+      .delete<TodoResponce>(`${environment.baseURL}/todo-lists/${deletedID}`, this.httpOptions)
+      .pipe(
+        catchError(TodosService.errorHandler.bind(this)),
+        map(() => {
+          return this.todos$.getValue().filter(td => td.id !== deletedID)
+        })
+      )
+      .subscribe(todos => {
+        this.todos$.next(todos)
+      })
   }
 }
