8dd7be567a73349c437990e7cba2eaec81cc070b
EgorKirillov <80296253508@mail.ru>
2022-10-31 09:59:26 +0300

    add: validators

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'tdl-login',
@@ -10,9 +10,19 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   loginForm = new FormGroup({
-    email: new FormControl('init email'),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required]),
   })
+
+  //спросить
+  get email() {
+    // return this.loginForm.controls.email
+    return this.loginForm.get('email')!
+  }
+
+  get password() {
+    return this.loginForm.get('password')!
+  }
 
   onSubmit() {
     console.log(this.loginForm.value)
