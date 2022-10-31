fce1a7fd695280142706c565dd956aa26d404989
EgorKirillov <80296253508@mail.ru>
2022-10-31 11:13:36 +0300

    add: routing structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TodosComponent } from './components/todos/todos.component'
 import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 import { LoginComponent } from './components/login/login.component'
+import { HomeComponent } from './components/home/home.component'
+import { UsersComponent } from './components/users/users.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    LoginComponent,
+    HomeComponent,
+    UsersComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
