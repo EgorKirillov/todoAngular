2af7c64a5a8105114cce5cb606deb69a54241710
EgorKirillov <80296253508@mail.ru>
2022-11-01 16:41:08 +0300

    refactor: auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,26 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TodosComponent } from '../todos/components/todos/todos.component'
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms'
-import { LoginComponent } from '../auth/components/login/login.component'
 import { HomeComponent } from '../home/components/home/home.component'
 import { UsersComponent } from '../users/components/users/users.component'
-import { ProfileComponent } from '../profile/components/profile/profile.component'
 import { PageNotFoundComponent } from '../page-not-found/components/page-not-found/page-not-found.component'
 import { MatButtonModule } from '@angular/material/button'
 import { CredentialsInterceptor } from '../core/interceptors/credentials.interceptor'
 import { SharedModule } from '../shared/shared.module'
+import { AuthModule } from '../auth/auth.module'
+import { ProfileModule } from '../profile/profile.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent,
-    LoginComponent,
-    HomeComponent,
-    UsersComponent,
-    ProfileComponent,
-    PageNotFoundComponent,
+  declarations: [AppComponent, TodosComponent, HomeComponent, UsersComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    SharedModule,
+    AuthModule,
+    ProfileModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, MatButtonModule, SharedModule],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
