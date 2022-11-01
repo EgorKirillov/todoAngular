f0d8bd4c2282372f5e547c33e7d01a1f9fa74686
EgorKirillov <80296253508@mail.ru>
2022-11-01 19:52:41 +0300

    fix: material import

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { UsersRoutingModule } from './users-routing.module'
 import { UsersComponent } from './components/users/users.component'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [UsersComponent],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, MatButtonModule],
 })
 export class UsersModule {}
