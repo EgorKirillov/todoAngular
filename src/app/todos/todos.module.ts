875c3a026c62d65d244ae24fcdc3a9ea771e1601
EgorKirillov <80296253508@mail.ru>
2022-11-07 14:32:31 +0300

    add: tasks to todos

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -5,9 +5,10 @@ import { TodosRoutingModule } from './todos-routing.module'
 import { TodosComponent } from './components/todos/todos.component'
 import { TodoComponent } from './components/todo/todo.component'
 import { SharedModule } from '../shared/shared.module'
+import { TasksModule } from '../tasks/tasks.module'
 
 @NgModule({
   declarations: [TodosComponent, TodoComponent],
-  imports: [CommonModule, TodosRoutingModule, SharedModule],
+  imports: [CommonModule, TodosRoutingModule, SharedModule, TasksModule],
 })
 export class TodosModule {}
