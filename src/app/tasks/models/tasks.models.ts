f96fffb0c3f413c56ba80aca0aa656c45734ead1
EgorKirillov <80296253508@mail.ru>
2022-11-09 18:27:46 +0300

    feat: add task models

--- src/app/tasks/models/tasks.models.ts
+++ src/app/tasks/models/tasks.models.ts
@@ -4,8 +4,8 @@ export interface Task {
   description: string
   todoListId: string
   order: number
-  status: number
-  priority: number
+  status: TaskStatuses
+  priority: TaskPriorities
   startDate: string
   deadline: string
   addedDate: string
@@ -23,4 +23,28 @@ export interface TasksResponce<T = { item: Task }> {
   data: T
   resultCode: number //   (0 if opearation completed successfullt, other numbers - some error occured)
   messages: string[] //  is empty if resultCode is 0, contains error messages if resultCode is different from 0
+}
+
+export enum TaskStatuses {
+  New,
+  InProgress,
+  Complited,
+  Draft,
+}
+
+export enum TaskPriorities {
+  Low,
+  Middle,
+  Hi,
+  Urgently,
+  Later,
+}
+
+export type UpdateTaskModelType = {
+  title: string
+  description: string
+  status: TaskStatuses
+  priority: TaskPriorities
+  startDate: string
+  deadline: string
 }
